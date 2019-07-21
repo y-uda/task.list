cad3705be2dbd413ef443a839f42b5d65fb5e4d9
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2019-07-22 00:53:04 +0900

    add name_null

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_125914) do
+ActiveRecord::Schema.define(version: 2019_07_21_154459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
